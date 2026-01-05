fafe7eb75809b6e7f02e9224cc776498c1fab4ba
btoumana <basmatoumanar55@gmail.com>
2026-01-05 17:33:29 +0100

    try

--- ball.js
+++ ball.js
@@ -16,8 +16,8 @@ let ballLaunched = false
 a.style.transform = `translate(${pabbleX}px, ${pabbleY}px)`
 
 let keyword = {
-    left: false,
-    right: false
+    left: 0,
+    right: 0
 }
 
 document.addEventListener("keydown", (e) => {
@@ -28,10 +28,8 @@ document.addEventListener("keydown", (e) => {
     }
     if (e.key === "ArrowRight") {
         keyword.right = true
-
     } else if (e.key === "ArrowLeft") {
         keyword.left = true
-
     }
 })
 document.addEventListener("keyup", (e) => {
@@ -67,27 +65,34 @@ let y = pabbleY - letterHeight
 // element.style.transform = `translate(${x}px, ${y}px)`
 let dx = 5
 let dy = 5
-
+let bricks = []
 let boardBricks = document.getElementById("board");
-
+let brickX = []
 let rows = 3;
 let cols = 11;
+let brickWidth = 40
+let brickHeight = 20
 
 for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < cols; j++) {
-    let brick = document.createElement("div");
-    brick.className = "brick";
+    for (let j = 0; j < cols; j++) {
+        let brick = document.createElement("div");
+        brick.className = "brick";
 
-    brick.style.width = "40px";
-    brick.style.height = "20px";
-    brick.style.backgroundColor = "brown";
-    brick.style.position = "absolute";
-
-    brick.style.left = 5 + (j * 45) + "px"; // !
-    brick.style.top = 10 + (i * 25) + "px";
-
-    boardBricks.appendChild(brick);
-  }
+        brick.style.width = "40px";
+        brick.style.height = "20px";
+        brick.style.backgroundColor = "saddlebrown";
+        brick.style.position = "absolute";
+        brick.style.left = 5 + (j * 45) + "px"; // == 90px
+        brick.style.top = 10 + (i * 25) + "px"; // == 20px
+        //console.log(brick.style.left)
+        bricks.push({
+            x: parseInt(brick.style.left),
+            y: parseInt(brick.style.top)
+        })
+        //console.log(brickXY)
+        boardBricks.appendChild(brick);
+    }
+    //brickY.push(brick.style.top)
 }
 
 
@@ -114,6 +119,19 @@ function ball() {
         let maxDX = 5
         dx = (contact - 0.5) * 2 * maxDX
     }
+    let contact = false
+    bricks.forEach(brick => {
+        if (
+            x < brick.x + brickWidth &&
+            x + letterWidth > brick.x &&
+            y < brick.y + brickHeight &&
+            y + letterHeight > brick.y
+        ) {
+            dy = -dy
+        }
+    })
+
+
 
 }
 
@@ -141,16 +159,39 @@ requestAnimationFrame(loop)
 
 
 /*
-requestAnimationFrame(loop)
+let ballX = '95px';
+let ballY = '10px';
 
-loop():
-    movePabble()
+bricksPos.forEach(pos => {
+  if (pos.x === ballX && pos.y === ballY) {
+    console.log("MATCH trouvé", pos);
+  }
+});
 
-    if balle NON lancée:
-        coller la balle à la raquette
-    else:
-        faire bouger la balle (dx / dy)
 
-    draw()
+
+let ballX = 95;
+let ballY = 10;
+
+bricksPos.forEach(pos => {
+  let x = parseInt(pos.x);
+  let y = parseInt(pos.y);
+
+  if (x === ballX && y === ballY) {
+    console.log("MATCH trouvé", pos);
+  }
+});
+
+
+let value = 10;
+
+bricksPos.forEach(pos => {
+  if (parseInt(pos.x) === value) {
+    console.log("match avec x", pos);
+  }
+  if (parseInt(pos.y) === value) {
+    console.log("match avec y", pos);
+  }
+});
 
 */
